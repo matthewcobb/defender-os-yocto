1a20489dc1658f86968977db4345e43af46f5343
Matt Cobb <matt@reedsy.com>
2025-04-24 11:05:45 +0100

    upodat

--- setup.sh
+++ setup.sh
@@ -7,32 +7,35 @@ echo "===================================="
 echo "This script will set up all required repositories for building."
 echo ""
 
+# Get the absolute path of the current directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_DIR}"
+
 # Function to clone or update a repo
 clone_or_update() {
     local repo_url=$1
     local branch=$2
     local dir_name=$3
 
-    if [ -d "$dir_name" ]; then
-        echo "Updating $dir_name..."
-        cd $dir_name
+    if [ -d "${SCRIPT_DIR}/${dir_name}" ]; then
+        echo "Updating ${dir_name}..."
+        cd "${SCRIPT_DIR}/${dir_name}"
         git pull
-        cd ..
+        cd "${SCRIPT_DIR}"
     else
-        echo "Cloning $dir_name..."
+        echo "Cloning ${dir_name}..."
         if [ -n "$branch" ] && [ "$branch" != "none" ]; then
-            git clone -b $branch $repo_url $dir_name
+            git clone -b $branch $repo_url "${SCRIPT_DIR}/${dir_name}"
         else
-            git clone $repo_url $dir_name
+            git clone $repo_url "${SCRIPT_DIR}/${dir_name}"
         fi
     fi
 }
 
 # Create a complete bblayers.conf
 create_bblayers_conf() {
-    mkdir -p conf
-    BASEDIR=$(pwd)
-    cat > conf/bblayers.conf << EOF
+    mkdir -p "${SCRIPT_DIR}/conf"
+    cat > "${SCRIPT_DIR}/conf/bblayers.conf" << EOF
 # POKY_BBLAYERS_CONF_VERSION is increased each time build/conf/bblayers.conf
 # changes incompatibly
 POKY_BBLAYERS_CONF_VERSION = "2"
@@ -41,24 +44,101 @@ BBPATH = "\${TOPDIR}"
 BBFILES ?= ""
 
 BBLAYERS ?= " \\
-  ${BASEDIR}/poky-source/meta \\
-  ${BASEDIR}/poky-source/meta-poky \\
-  ${BASEDIR}/poky-source/meta-yocto-bsp \\
-  ${BASEDIR}/meta-openembedded/meta-oe \\
-  ${BASEDIR}/meta-openembedded/meta-multimedia \\
-  ${BASEDIR}/meta-openembedded/meta-networking \\
-  ${BASEDIR}/meta-openembedded/meta-python \\
-  ${BASEDIR}/meta-openembedded/meta-filesystems \\
-  ${BASEDIR}/meta-qt6 \\
-  ${BASEDIR}/meta-clang \\
-  ${BASEDIR}/meta-rust \\
-  ${BASEDIR}/meta-browser/meta-chromium \\
-  ${BASEDIR}/meta-virtualization \\
-  ${BASEDIR}/meta-raspberrypi \\
-  ${BASEDIR}/meta-defender \\
+  ${SCRIPT_DIR}/poky-source/meta \\
+  ${SCRIPT_DIR}/poky-source/meta-poky \\
+  ${SCRIPT_DIR}/poky-source/meta-yocto-bsp \\
+  ${SCRIPT_DIR}/meta-openembedded/meta-oe \\
+  ${SCRIPT_DIR}/meta-openembedded/meta-multimedia \\
+  ${SCRIPT_DIR}/meta-openembedded/meta-networking \\
+  ${SCRIPT_DIR}/meta-openembedded/meta-python \\
+  ${SCRIPT_DIR}/meta-openembedded/meta-filesystems \\
+  ${SCRIPT_DIR}/meta-qt6 \\
+  ${SCRIPT_DIR}/meta-clang \\
+  ${SCRIPT_DIR}/meta-rust \\
+  ${SCRIPT_DIR}/meta-browser/meta-chromium \\
+  ${SCRIPT_DIR}/meta-virtualization \\
+  ${SCRIPT_DIR}/meta-raspberrypi \\
+  ${SCRIPT_DIR}/meta-defender \\
   "
 EOF
-    echo "Created conf/bblayers.conf with all required layers"
+    echo "Created conf/bblayers.conf with all required layers (using absolute paths)"
+}
+
+# Create or update meta-defender layer if it doesn't exist
+create_meta_defender() {
+    if [ ! -d "${SCRIPT_DIR}/meta-defender" ]; then
+        echo "Creating meta-defender layer..."
+        mkdir -p "${SCRIPT_DIR}/meta-defender/conf"
+        mkdir -p "${SCRIPT_DIR}/meta-defender/recipes-core/images"
+        mkdir -p "${SCRIPT_DIR}/meta-defender/recipes-automotive"
+
+        # Create basic layer configuration
+        cat > "${SCRIPT_DIR}/meta-defender/conf/layer.conf" << EOF
+# We have a conf and classes directory, add to BBPATH
+BBPATH .= ":${LAYERDIR}"
+
+# We have recipes-* directories, add to BBFILES
+BBFILES += "${LAYERDIR}/recipes-*/*/*.bb \\
+            ${LAYERDIR}/recipes-*/*/*.bbappend"
+
+BBFILE_COLLECTIONS += "defender"
+BBFILE_PATTERN_defender = "^${LAYERDIR}/"
+BBFILE_PRIORITY_defender = "10"
+
+LAYERDEPENDS_defender = "core qt6-layer openembedded-layer"
+LAYERSERIES_COMPAT_defender = "kirkstone"
+EOF
+
+        # Create a simple image recipe
+        cat > "${SCRIPT_DIR}/meta-defender/recipes-core/images/defender-automotive-image.bb" << EOF
+SUMMARY = "Defender Automotive In-Car Entertainment System"
+DESCRIPTION = "Custom image for in-car entertainment system featuring Qt UI, CarPlay support, and automotive dashboard"
+LICENSE = "MIT"
+
+inherit core-image
+
+IMAGE_FEATURES += " \\
+    debug-tweaks \\
+    splash \\
+    package-management \\
+    ssh-server-openssh \\
+    tools-debug \\
+    tools-profile \\
+    tools-sdk \\
+"
+
+# Base packages
+IMAGE_INSTALL = " \\
+    packagegroup-core-boot \\
+    packagegroup-core-full-cmdline \\
+    kernel-modules \\
+    linux-firmware \\
+    openssh-sftp-server \\
+    htop \\
+    nano \\
+    usbutils \\
+    pciutils \\
+    wireless-tools \\
+    networkmanager \\
+"
+
+# Wayland compositor
+IMAGE_INSTALL += " \\
+    wayland \\
+    weston \\
+    weston-init \\
+"
+
+# Set the root password to 'defender'
+inherit extrausers
+EXTRA_USERS_PARAMS = "usermod -P defender root;"
+
+# Image size
+IMAGE_ROOTFS_SIZE ?= "8192"
+IMAGE_ROOTFS_EXTRA_SPACE_append = "\${@bb.utils.contains("DISTRO_FEATURES", "systemd", " + 4096", "", d)}"
+EOF
+        echo "Created meta-defender layer with basic image recipe"
+    fi
 }
 
 # Clone all repositories
@@ -88,11 +168,17 @@ clone_or_update "https://github.com/OSSystems/meta-browser.git" "kirkstone" "met
 # Raspberry Pi
 clone_or_update "https://git.yoctoproject.org/meta-raspberrypi" "kirkstone" "meta-raspberrypi"
 
+# Create meta-defender layer if it doesn't exist
+create_meta_defender
+
 # Create build directory structure if it doesn't exist
-mkdir -p build
+mkdir -p "${SCRIPT_DIR}/build"
 
 # Create bblayers.conf that will be used by build.sh
 create_bblayers_conf
+
+# Make both scripts executable
+chmod +x "${SCRIPT_DIR}/setup.sh" "${SCRIPT_DIR}/build.sh"
 
 echo ""
 echo "Setup complete!"
